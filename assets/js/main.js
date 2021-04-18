9325966bc58b97459eb4b8418b63db3bc649aacf
NeaCS <naldi.castro@gmail.com>
2021-04-18 12:38:15 -0400

    subiendo proyecto

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    //Smooth scrolling
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+        //tooltip
+        $('[data-toggle="tooltip"]').tooltip()
+
+});
